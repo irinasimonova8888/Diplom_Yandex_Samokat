dac7b13a2db4f02a87f8b569955f9e27b7a0d1b0
irinasimonova8888 <irina.simonova8888@gmail.com>
2026-01-09 16:22:18 +0300

    Changes after review

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
 # Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
 # Пример значения: "https://api.example.com"
-URL_SERVICE = "https://1ef65229-9b59-43e8-80cd-7c5b71024a5c.serverhub.praktikum-services.ru"
+URL_SERVICE = " https://4bd29182-4f63-47cb-b5f4-26b554cb4f02.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER = "/api/v1/orders/track"
